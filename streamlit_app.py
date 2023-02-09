719f6a944bf41f6f114ded50c3d1998e915e71c6
Spoorthy <43513220+spoorthyvv@users.noreply.github.com>
2023-02-09 11:04:36 +0530

    Update streamlit_app.py
    done2

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,3 +7,5 @@ streamlit.text('🥣🥣🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text(' 🥗 🥗 🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 🐔 🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocardo shit')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
