f827f372f48ab9843c8ea9dd96b444b8e87276e3
Spoorthy <43513220+spoorthyvv@users.noreply.github.com>
2023-02-09 11:03:11 +0530

    Update streamlit_app.py
    done

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,7 @@ streamlit.title('My parents new healthy diner Which is gonna piss my dad off')
 
 
 streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.text('🥣🥣🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text(' 🥗 🥗 🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 🐔 🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocardo shit')
