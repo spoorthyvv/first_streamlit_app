fda21c31d323c5c86de6612d7ec2589d88b16869
Spoorthy <43513220+spoorthyvv@users.noreply.github.com>
2023-02-09 23:50:59 +0530

    Update streamlit_app.py
    hh

--- streamlit_app.py
+++ streamlit_app.py
@@ -33,6 +33,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+fruityvice_response=requests.get("https://fruityvice.com/api/fruit/" +fruit_choice)
 
 
 
